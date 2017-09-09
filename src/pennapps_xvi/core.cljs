1e007c47e0e3724e60a2026d625a792b973ab17a
Zhouyu Qian <qzy@qzy.io>
2017-09-09 17:28:49 -0400

    Functional navbar

--- src/pennapps_xvi/core.cljs
+++ src/pennapps_xvi/core.cljs
@@ -6,7 +6,9 @@
 ;; Views
 
 (defn overview []
-  [:h2 "Overview"])
+  [:div
+   [:h2 "Overview"]
+   [:h3 "Your Investment Portfolio"]])
 
 (defn investments []
   [:h2 "Long-Term Investments"])
@@ -26,26 +28,28 @@
 (def tabs
   (keys tabmap))
 
-(defn body [selected-tab]
+(defonce current-tab (r/atom "Overview"))
+
+(defn body []
   [:div {:id "body"}
    [:div#sidebar
     [:ul
-     (map (fn [t] [:li {:key t} [:a {:class (if (= selected-tab t) "selected" "")} t]]) tabs)]]
+     (map (fn [t] [:li {:key t} [:a {:on-click #(do
+                                                  (swap! current-tab (fn [_] t))) :class (if (= @current-tab t) "selected" "")} t]]) tabs)]]
    [:div#main-content
-    [(get tabmap selected-tab)]
-    ]])
+    [(get tabmap @current-tab "Overview")]]])
 
 (defn home-page []
-  [:div#container
-   [:div#header
-    [:img#logo {:src "img/logo.png" :height 64 :width 65}]
-    [:p#title "Stock Advisors"]
-    [:p#menu "Welcome Johnson! "
-     [:button "Logout"]]]
-   [body "Overview"]
-   [:div#footer
-    [:p [:strong "Copyright (c) 2017 Stock Advisors and Company. All rights reserved."]]
-    [:p [:strong "Important Disclaimer: "] "Any past performance, projection, forecast or simulation of results is not necessarily indicative of the future or likely performance of any company or investment. Investors are responsible for paying all taxes imposed by relevant authorities on any interests received and the amount of tax payable is dependent on individual circumstances. The information provided does not take into account your specific investment objectives, financial situation or particular needs. Before you act on any information on this site, you should always seek independent financial, tax, and legal advice or make such independent investigations as you consider necessary or appropriate regarding the suitability of the investment product, taking into account your specific investment objectives, financial situation or particular needs."]]]
+      [:div#container
+     [:div#header
+      [:img#logo {:src "img/logo.png" :height 64 :width 65}]
+      [:p#title "Stock Advisors"]
+      [:p#menu "Welcome Johnson! "
+       [:button "Logout"]]]
+     [body]
+     [:div#footer
+      [:p [:strong "Copyright (c) 2017 Stock Advisors and Company. All rights reserved."]]
+      [:p [:strong "Important Disclaimer: "] "Any past performance, projection, forecast or simulation of results is not necessarily indicative of the future or likely performance of any company or investment. Investors are responsible for paying all taxes imposed by relevant authorities on any interests received and the amount of tax payable is dependent on individual circumstances. The information provided does not take into account your specific investment objectives, financial situation or particular needs. Before you act on any information on this site, you should always seek independent financial, tax, and legal advice or make such independent investigations as you consider necessary or appropriate regarding the suitability of the investment product, taking into account your specific investment objectives, financial situation or particular needs."]]]
   )
 
 ;; -------------------------
