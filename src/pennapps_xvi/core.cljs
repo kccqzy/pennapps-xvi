88d00bdabedccf799a97b87f95f6a92ab99810f6
Zhouyu Qian <qzy@qzy.io>
2017-09-10 02:46:07 -0400

    Abstract over widths/heights

--- src/pennapps_xvi/core.cljs
+++ src/pennapps_xvi/core.cljs
@@ -92,21 +92,30 @@
    {:component-did-mount
     (fn [this]
       (let [raw-data (gen-dummy-stock-data 100)
-            width 890
-            height 450
+            margin-top 20
+            margin-right 20
+            margin-bottom 30
+            margin-left 50
+            width (- 960 margin-left margin-right)
+            height (- 500 margin-top margin-bottom)
             x (-> js/techan.scale (.financetime) (.range (clj->js [0 width])))
             y (-> js/d3 (.scaleLinear) (.range (clj->js [height 0])))
             candlestick (-> js/techan.plot (.candlestick) (.xScale x) (.yScale y))
             xAxis (-> js/d3 (.axisBottom) (.scale x))
             yAxis (-> js/d3 (.axisLeft) (.scale y))
-            svg (-> js/d3 (.select "div#candlestick") (.append "svg") (.attr "width" 960) (.attr "height" 500) (.append "g")
-                    (.attr "transform" "translate(50,20)"))
+            svg (-> js/d3
+                    (.select "div#candlestick")
+                    (.append "svg")
+                    (.attr "width" (+ width margin-left margin-right))
+                    (.attr "height" (+ height margin-top margin-bottom))
+                    (.append "g")
+                    (.attr "transform" (str "translate(" margin-left "," margin-top ")")))
             accessor (-> candlestick (.accessor))
             data (-> (clj->js raw-data) (.sort (fn [a b] (js/d3.ascending (.d accessor a) (.d accessor b)))))
             ]
         (js/console.log data)
         (-> svg (.append "g") (.attr "class" "candlestick"))
-        (-> svg (.append "g") (.attr "class" "x axis") (.attr "transform" "translate(0, 450)"))
+        (-> svg (.append "g") (.attr "class" "x axis") (.attr "transform" (str "translate(0," height ")")))
         (-> svg (.append "g") (.attr "class" "y axis") (.append "text") (.attr "transform" "rotate(-90)") (.attr "y" "6")
             (.attr "dy" ".71em") (.style "text-anchor" "end") (.text "Price ($)"))
         (.domain x (.map data (.-d (-> candlestick (.accessor)))))
