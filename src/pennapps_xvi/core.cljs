323bf6d3c3a367265d49fbec124b8a5e4a320fe5
Zhouyu Qian <qzy@qzy.io>
2017-09-09 21:30:57 -0400

    Add "friends' picks"

--- src/pennapps_xvi/core.cljs
+++ src/pennapps_xvi/core.cljs
@@ -1,14 +1,59 @@
 (ns pennapps-xvi.core
     (:require
+     [clojure.core]
       [reagent.core :as r]))
+
 
 ;; -------------------------
 ;; Views
 
+(def dummy-portfolio-data
+  [
+   ["U.S. Stocks" "11"]
+   ["U.S. Bonds" "22"]
+   ["Cryptocurrencies" "19"]
+   ["Energy" "40"]
+   ["International Stock" "8"]
+   ])
+
+(def friend-list
+  [
+   ["1" "Jennifer" "AAPL"]
+   ["2" "Jake" "GOOG"]
+   ["3" "Mike" "AMZN"]
+   ["4" "Sam" "MSFT"]
+   ["5" "Tim" "VTSMX"]
+   ["6" "Julie" "F"]
+   ["7" "Harry" "SYMC"]
+   ["8" "Jolene" "XOM"]
+   ["9" "William" "BLK"]
+   ["10" "Elaina" "BLK"]
+   ["11" "Shu" "CSCO"]
+   ["12" "Irene" "FDX"]
+   ["13" "Kelly" "FB"]])
+
 (defn overview []
-  [:div
-   [:h2 "Overview"]
-   [:h3 "Your Investment Portfolio"]])
+  (r/create-class
+   {:component-did-mount
+    (fn [this]
+      (js/c3.generate (clj->js {"bindto" "#portfolio-composition"
+                                "data" {"columns" dummy-portfolio-data
+                                        "type" "donut"}}))
+      )
+    :reagent-render
+    (fn [_] [:div
+             [:h2 "Overview"]
+             [:h3 "Your Investment Portfolio"]
+             [:div#portfolio-composition]
+             [:h3 "Friends\u2019 Picks"]
+             [:div#stock-picks
+              (map (fn [[id name pick]]
+                     [:div.stock-pick {:key name}
+                      [:div [:img {:src (str "img/profile/pic-" id ".jpg")}]]
+                      [:div.name name]
+                      [:div.pick pick]])
+                   (take 5 (clojure.core/shuffle friend-list)))]
+             ])}))
 
 (defn investments []
   [:h2 "Long-Term Investments"])
@@ -34,8 +79,8 @@
   [:div {:id "body"}
    [:div#sidebar
     [:ul
-     (map (fn [t] [:li {:key t} [:a {:on-click #(do
-                                                  (swap! current-tab (fn [_] t))) :class (if (= @current-tab t) "selected" "")} t]]) tabs)]]
+     (map (fn [t] [:li {:key t} [:a {:on-click #(swap! current-tab (fn [_] t))
+                                     :class (if (= @current-tab t) "selected" "")} t]]) tabs)]]
    [:div#main-content
     [(get tabmap @current-tab "Overview")]]])
 
